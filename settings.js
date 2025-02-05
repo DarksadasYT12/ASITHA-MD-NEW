46fa51a584ff6a472d5784cfdd7eddc88187889f
Darksadas YT <kollaamla@gmail.com>
2025-02-05 21:05:07 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=yiJ2xaqY#QkEF7DR3b6B6nF0dKA1CrgLpx2EClDCJOvQdVU8Cnc8' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=arQzhQzJ#FkTItxGcOvWBSH8dBzldYH81Oia72l4qiZB4soBCt-A' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SUDO: process.env.SUDO === undefined ? '9473737373,9363636367' : process.env.SUDO,
